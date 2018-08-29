f40034c1c254370046342026d2f01723ca2bf822
localadmin <megatcorleone000@gmail.com>
2018-08-29 09:55:04 +0800

    responsiveness

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser');
+
+const request = require('request');
+const apiKey = '6f7421152f141c7c74a439b6b1078c61';
+
+
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 const app = express()
@@ -10,14 +16,77 @@ const pool = new Pool({
 });
 
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .get('/cool', (req, res) => res.send(cool()))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-  //public api
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.static('public'));
+  app.set('view engine', 'ejs')
 
-  console.log("Hello World!")
+  // app.get('/', (req, res) => res.render('pages/index'))
+  app.get('/', function (req, res) {
+
+        let city = req.body.city;
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+    // let weather
+    // res.render('pages/index', {
+    //   weather: null,
+    //   error: 'Error, please try again'
+    // });
+
+    request(url, function (err, response, body) {
+        console.log(body)
+        if(err){
+          console.log(12345)
+          res.render('pages.index', {weather: null, error: 'Error, please try again'});
+        } else {
+          let weather = JSON.parse(body)
+
+          if(weather.main == undefined){
+            console.log(123)
+            res.render('pages/index', {weather: null, error: 'Error, please try again'});
+          } else {
+            console.log(31)
+            let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+            res.render('pages/index', {weather: weatherText, error: null});
+          }
+        }
+      });
+  })
+
+  app.listen(PORT, function () {
+    console.log('Example app listening on port 500!')
+  })
+
+
+
+  app.post('/', function (req, res) {
+
+      let city = req.body.city;
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+    request(url, function (err, response, body) {
+        console.log(body)
+        if(err){
+          console.log(12345)
+          res.render('pages/index', {weather: null, error: 'Error, please try again'});
+        } else {
+          let weather = JSON.parse(body)
+
+          if(weather.main == undefined){
+            console.log(123)
+            res.render('pages/index', {weather: null, error: 'Error, please try again'});
+          } else {
+            console.log(31)
+            let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+            res.render('pages/index', {weather: weatherText, error: null});
+          }
+        }
+      });
+  });
